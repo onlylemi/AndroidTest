aca16f11b1a29e9f8c2e9cb55952184ef11c89f9
onlylemi <xiaomi_0623@163.com>
2016-07-23 10:44:35 +0800

    test sliding finish activity lifecycle

--- Test_SildingFinish/app/src/main/java/com/onlylemi/test_sildingfinish/SecondActivity.java
+++ Test_SildingFinish/app/src/main/java/com/onlylemi/test_sildingfinish/SecondActivity.java
@@ -1,12 +1,73 @@
 package com.onlylemi.test_sildingfinish;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SecondActivity extends SlidingFinishActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: ");
         setContentView(R.layout.activity_second);
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, ThirdActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState: ");
     }
 }
---
+++ Test_SildingFinish/app/src/main/java/com/onlylemi/test_sildingfinish/ThirdActivity.java
@@ -0,0 +1,12 @@
+package com.onlylemi.test_sildingfinish;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends SlidingFinishActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
