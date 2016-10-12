30cbba6d3614e4542286fa748e6e76026c016522
onlylemi <onlylemi.com@gmail.com>
2016-10-12 18:22:37 +0800

    check notification state

--- NotificationTest/app/src/main/java/com/onlylemi/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/onlylemi/notificationtest/MainActivity.java
@@ -1,20 +1,26 @@
 package com.onlylemi.notificationtest;
 
 import android.app.AppOpsManager;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private Button mButton;
     private Button mButton1;
@@ -53,17 +59,39 @@ public class MainActivity extends AppCompatActivity {
 //                        .setFullScreenIntent(pendingIntent, false)
                         .setAutoCancel(true);
 
+                Notification notification = mBuilder.build();
+                notification.defaults |= Notification.DEFAULT_SOUND;
+                notification.defaults |= Notification.DEFAULT_VIBRATE;
+                notification.defaults |= Notification.DEFAULT_LIGHTS;
+                notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+                notification.flags |= Notification.FLAG_INSISTENT;
+
                 NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 //                mNotificationManager.notify(new Random().nextInt(100), mBuilder.build());
-                mNotificationManager.notify(new Random().nextInt(100), mBuilder.build());
+                mNotificationManager.notify(new Random().nextInt(100), notification);
             }
         });
 
         mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AppOpsManager aom = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+                Integer op = JavaCalls.getStaticField(AppOpsManager.class, "OP_POST_NOTIFICATION");
+                if (op == null) {
+                    Log.i(TAG, "onClick: unknown");
+                }
+                try {
+                    ApplicationInfo info = getPackageManager().getApplicationInfo(getPackageName(), 0);
+                    AppOpsManager mAppOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+                    Integer mode = JavaCalls.callMethod(mAppOps, "checkOpNoThrow", op, info.uid, getPackageName());
 
+                    if (mode != null && mode == 00) {
+                        Log.i(TAG, "onClick: allowed");
+                    }else {
+                        Log.i(TAG, "onClick: not_allowed");
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
